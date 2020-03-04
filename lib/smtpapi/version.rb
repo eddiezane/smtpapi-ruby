ed78ba5bbb7465ccfc99274c23eff19cc9af67ec
Twilio <twilio-ci@twilio.com>
2020-03-04 21:25:56 +0000

    Release 0.1.5

--- lib/smtpapi/version.rb
+++ lib/smtpapi/version.rb
@@ -2,5 +2,5 @@
 # SendGrid smtpapi
 #
 module Smtpapi
-  VERSION = '0.1.4'.freeze
+  VERSION = '0.1.5'.freeze
 end
--- test/test.rb
+++ test/test.rb
@@ -7,7 +7,7 @@ require './lib/smtpapi'
 #
 class SmtpapiTest < Test::Unit::TestCase
   def test_version
-    assert_equal('0.1.4', Smtpapi::VERSION)
+    assert_equal('0.1.5', Smtpapi::VERSION)
   end
 
   def test_empty
